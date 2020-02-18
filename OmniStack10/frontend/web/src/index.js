f18e62d5dac691b033b09f3d7f2f9dab6a5e9278
Victor Radael <victorradael@hotmail.com>
2020-02-18 18:07:57 -0300

    ATT REPOS

---
+++ OmniStack10/frontend/web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root')); // Desenhando o App dentro do Body com id='root
+//JSX (JavaScript + HTML)
